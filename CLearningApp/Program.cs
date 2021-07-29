5e25592d9376e5baf0885f38d82005ba678eedae
Raisa Kulakovska <rkula@softserveinc.com>
2021-07-29 09:13:03 +0300

    project created

---
+++ CLearningApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLearningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
