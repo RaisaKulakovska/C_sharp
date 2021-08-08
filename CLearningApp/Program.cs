354a9e92e3a5950049b6d8797ba7b4895ed64655
Raisa Kulakovska <rkula@softserveinc.com>
2021-08-08 17:05:31 +0300

    commit from master, second line added

--- CLearningApp/Program.cs
+++ CLearningApp/Program.cs
@@ -7,6 +7,7 @@ namespace CLearningApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Master branch");
         }
     }
 }
